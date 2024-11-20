2470ab55a2218b7a7de375150bab8933be45b2ab
nikhil6392 <nikhilpathak210431@gmail.com>
2024-11-21 02:59:53 +0530

    styling the Dating cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from'./component/Header.js'
+import DatingCards from './component/DatingCards.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      < DatingCards />
     </div>
   );
 }
--- src/component/DatingCards.js
+++ src/component/DatingCards.js
@@ -11,10 +11,14 @@ const DatingCards = () => {
             name: "Another Guy", imgUrl: "https://images.unsplash.com/photo-1547508446-e097049cade7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
-            name: "Random Girl", imgUrl: "https://unsplash.com/photos/a-woman-with-blonde-hair-wearing-a-black-and-white-dress-u0TFS_wnqPE"
+            name: "Random Girl", imgUrl: "https://plus.unsplash.com/premium_photo-1668319914124-57301e0a1850?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Z2lybHxlbnwwfHwwfHx8MA%3D%3D"
         },
         {
             name: "Another Girl", imgUrl: "https://images.unsplash.com/photo-1596245195341-b33a7f275fdb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGdpcmx8ZW58MHx8MHx8fDA%3D"
+        },
+        {
+            name: "Other Girl",
+            imgUrl: "https://plus.unsplash.com/premium_photo-1661438493857-d0ff5ae08f1a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGdpcmx8ZW58MHx8MHx8fDA%3D"
         }
     ])
 
@@ -43,3 +47,5 @@ const DatingCards = () => {
         </div>
     )
 }
+
+export default DatingCards
