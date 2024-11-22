39e485bd5bd0e6321b57c735353cde65c3d18536
nikhil6392 <nikhilpathak210431@gmail.com>
2024-11-22 06:33:30 +0530

    styling the swipeButtons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from'./component/Header.js'
 import DatingCards from './component/DatingCards.js';
+import SwipeButtons from './component/SwipeButtons.js';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       < DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
